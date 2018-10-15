1eeb272fd1b99d19b4716eac7b780adc5298da9d
Kyle Conroy <kyle@kyleconroy.com>
2018-10-15 15:13:28 -0700

    Publish ithub-com-kevinburke-differ bdfd927653c820b3b7074bacc1fc05919a9e45de

---
+++ Formula/ithub-com-kevinburke-differ.rb
@@ -0,0 +1,12 @@
+class IthubComKevinburkeDiffer < Formula
+  desc ''
+  homepage 'http://dl.equinox.test:8000//ithub-com-kevinburke-differ'
+
+  url 'http://bin.equinox.test:8000/a/ha29bipaytp/ithub-com-kevinburke-differ-bdfd927653c820b3b7074bacc1fc05919a9e45de-darwin-amd64.tar.gz'
+  sha256 'c23eb132c1d9470c1faaea04b3ead5caf92dc5bb9dab4240b9c0196782bc0834'
+  version 'bdfd927653c820b3b7074bacc1fc05919a9e45de'
+
+  def install
+    bin.install 'differ'
+  end
+end
